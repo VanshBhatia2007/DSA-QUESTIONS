aac10ff0486d560ffff607ff1c5ec78ed275463e
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-08-09 15:46:59 +0530

    largest number

---
+++ Questions/larget-no.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <climits> // FOR USING INT_MIN & INT_MAX
+using namespace std;
+int main() {
+    int i , n , l , no;
+    l = INT_MIN;
+    cin >> n;
+    i = 1;
+    while(i<=n){
+        cin >> no;
+        if(no>l){
+            l = no;
+        }
+        i=i+1;
+    }
+    cout << "largest number is:" << l;
+    return 0;
+}
