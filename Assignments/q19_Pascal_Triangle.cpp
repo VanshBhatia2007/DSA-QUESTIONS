245a14e6313aa5c065b4da4a80a894d6c021e1d6
Vansh Bhatia <168754972+VanshBhatia2007@users.noreply.github.com>
2025-08-29 00:49:25 +0530

    added pascal triange

--- Assignments/q19_Pascal_Triangle.cpp
+++ Assignments/q19_Pascal_Triangle.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
+    int n,ncr=1;
     cin>>n;
     for(int r=0;r<n;r++){
-        int pt=1;
-        cout<<pt<<" ";
-        for(int i=1;i<=n;i++){
-            int ct=pt*(r-i+1)/i;
-            cout<<ct<<" ";
-            pt=ct;
+        int ncr=1;
+        for(int i=0;i<=r;i++){
+             cout<<ncr<<" ";
+
+            ncr =(((r-i)*ncr)/(i+1));
         }
     cout<<endl;
     }
